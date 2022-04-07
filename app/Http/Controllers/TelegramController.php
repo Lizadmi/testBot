a8a0427702f18ed2293ab18335ab4c228dea0f2b
кабинет№10 <liza.dmitreva00@mail.ru>
2022-04-07 13:26:15 +0500

    htaccess change

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -3,25 +3,63 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 
 class TelegramController extends Controller
 {
     public function getUpdates()
     {
-        $lastupdate = '41304312';
+        $lastMessage = "";
 
         $params = [
-            'offset' => $lastupdate,
+            "offset"=>$lastMessage,
         ];
 
-        $response = (json_decode(\Illuminate\Support\Facades\Http::get("https://api.telegram.org/bot".config('bot.bot')."/getUpdates"), JSON_OBJECT_AS_ARRAY));
+        $keyboard = [
+            "keyboard" => [
+                [
+                    ["text" => "Ввести имя"],
+                    ["text" => "Текущая температура воздуха МСК"],
+                    ["text" => "Список пользователей"],
+                ]
+            ],
+            "one_time_keyboard" => true,
+            "resize_keyboard" => true
+        ];
 
-        foreach ($response["result"] as $resp){
+        $weather =  json_decode(\Illuminate\Support\Facades\Http::get("http://api.weatherapi.com/v1/current.json",[
+            "key"=>"5dec626e40c44d9193471014220704",
+            "q"=>"Moscow"
+        ]),JSON_OBJECT_AS_ARRAY);
+
+        $temperature = $weather["current"]["temp_c"];
+
+
+
+        $response = (json_decode(\Illuminate\Support\Facades\Http::get("https://api.telegram.org/bot" . config('bot.bot') . "/getUpdates"), JSON_OBJECT_AS_ARRAY));
+
+        dd($response["result"]);
+
+        foreach ($response["result"] as $resp) {
             var_dump($resp);
-            echo $resp["message"]["chat"]["id"]."   ";
-            echo $resp["message"]["text"]."<br>";
-            echo $resp["message"]["text"]."<br>";
+//            echo $resp["message"]["from"]["first_name"]."<br>";
+//            echo $resp["message"]["from"]["last_name"]."<br>";
+//            echo $resp["message"]["from"]["username"]."<br><br>";
 
+//            if ($resp["message"]["text"] == "/start") {
+//                return \Illuminate\Support\Facades\Http::attach('photo', Storage::get('/public/pngTest.png'), 'pngTest.png')->post("https://api.telegram.org/bot" . config('bot.bot') . "/sendPhoto", [
+//                    'chat_id' => $resp["message"]["chat"]["id"],
+//                    'caption' => "Hello",
+//                    'reply_markup' => json_encode($keyboard)
+//                ]);
+//            }
+//            if ($resp["message"]["text"] == "Текущая температура воздуха МСК") {
+//                return \Illuminate\Support\Facades\Http::post("https://api.telegram.org/bot" . config('bot.bot') . "/sendMessage", [
+//                    'chat_id' => $resp["message"]["chat"]["id"],
+//                    'text' => "$temperature",
+//                ]);
+//            }
         }
     }
 }
